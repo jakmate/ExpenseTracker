16b20056c1f388c451b0fde8c255146e9ae6902e
jakmate <jakub.orzolek04@gmail.com>
2025-05-14 14:23:21 +0100

    config.hosts

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -88,5 +88,6 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.hosts << "expense-tracker-api-rnt7.onrender.com"
   config.hosts << "expense-tracker-three-sable-48.vercel.app"
 end
