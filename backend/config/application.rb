5d9a9104a0aa75ca2477464a50c10d22e4fef6c2
jakmate <jakub.orzolek04@gmail.com>
2025-05-14 11:36:41 +0100

    Deployment?

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -20,7 +20,7 @@ module Backend
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins "localhost:5173", "127.0.0.1:5173"
+        origins ENV['CORS_ORIGINS'] || 'https://your-app-name.vercel.app'
         resource "*",
                  headers: :any,
                  methods: %i[get post put patch delete options head],
