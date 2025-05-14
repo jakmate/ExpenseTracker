fc718b66f769343053d83050437e55afeaf4166b
jakmate <jakub.orzolek04@gmail.com>
2025-05-14 16:20:52 +0100

    README.md and reseeding issue on deployment

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,6 +3,10 @@ set -e
 
 rm -f /app/tmp/pids/server.pid
 
-bundle exec rails db:create db:migrate db:seed
+if [ "$RAILS_ENV" = "development" ]; then
+  bundle exec rails db:create db:migrate db:seed
+else
+  bundle exec rails db:migrate
+fi
 
 exec "$@"
